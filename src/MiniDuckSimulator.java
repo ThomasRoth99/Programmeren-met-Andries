aea6e51746bfdcf62b6ebc759611f9cbe2d6c61e
Thomas Roth <roth0005@hz.nl>
2018-04-09 14:07:39 +0200

    Eenden v1.1

--- src/MiniDuckSimulator.java
+++ src/MiniDuckSimulator.java
@@ -3,5 +3,9 @@ public class MiniDuckSimulator {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
     }
 }
